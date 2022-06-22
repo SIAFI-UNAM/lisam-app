aeea6eefdbaee547fa63d304aafed74e180c83ec
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-06-22 09:45:06 -0500

    no signs alert

--- lib/pages/capture_sign_page/widgets/letters_tile.widget.dart
+++ lib/pages/capture_sign_page/widgets/letters_tile.widget.dart
@@ -8,15 +8,33 @@ class LettersTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const textColor = Colors.blueGrey;
+    const textStyle = TextStyle(
+      fontSize: 18,
+      color: textColor,
+    );
+
+    if (letters.isEmpty) {
+      return Center(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              'Aún no se han reconocido señas',
+              style: textStyle,
+            ),
+          ],
+        ),
+      );
+    }
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
           "Letras:",
-          style: TextStyle(
-            fontSize: 18,
-            color: textColor,
-          ),
+          style: textStyle,
         ),
         const SizedBox(height: 10),
         Wrap(
