f226e26531904e3d59f6665d6b5854527cc57735
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-07-03 18:00:43 -0500

    v1 production

--- lib/environment/environment.dart
+++ lib/environment/environment.dart
@@ -1,4 +1,4 @@
 class Environment {
-  static const String url = 'http://10.0.2.2:5000';
+  static const String url = 'https://lisam-api.herokuapp.com';
   static const String apiPath = '$url/api/v1';
 }
